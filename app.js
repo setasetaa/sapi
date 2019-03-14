f1a8de4cd5ebbd9f14dad76e6167e28082b06fd5
setaseta <setasetaa@gmail.com>
2019-03-14 21:34:33 +0900

    20190314-3

--- app.js
+++ app.js
@@ -45,11 +45,7 @@ app.use('/dti/form', dtiFormRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  if(storage.getItem('accessToken') != null){
-    res.render("index");
-  }else{
-    res.render("user/login");
-  }
+  
   next();
 });
 
--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,14 @@ const storage = require('sessionstorage');
 
 
 router.get('/', function(req, res, next) {
-  //console.log(storage.getItem('accessToken'));
+  console.log(storage.getItem('accessToken'));
   if(storage.getItem('accessToken') != null){
     res.render("index");
   }else{
     res.render("user/login");
   }
+  res.render("index");
+
 });
 
 module.exports = router;
